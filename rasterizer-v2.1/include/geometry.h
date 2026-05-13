1f7911539ca3ba351cdf174eafe15c1138390099
William Hudson Taylor Tosh <whudsontosh@gmail.com>
2026-05-13 14:59:22 -0600

    Add files via upload
    Fixed Artifacts and Added the Ability to Place and Break Voxels

---
+++ rasterizer-v2.1/include/geometry.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "math.h"
+#include <vector>
+
+const int CHUNK_SIZE = 16;
+
+struct Vertex {
+    Vec3 position;
+    Vec3 color;
+};
+
+// A triangle made of 3 vertices
+struct Triangle {
+    Vertex v0, v1, v2;
+};
+
+struct Cube {
+    Vec3 position;
+    Vec3 color;
+    Triangle faces[12];
+};
+
+Cube createCube(Vec3 position, Vec3 color);
+
+struct Chunk {
+    Vec3 origin;
+    bool voxels[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
+    Vec3 colors[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
+
+    std::vector<Triangle> triangles; // precomputed visible triangles
+};
+
+Chunk createChunk(Vec3 origin);
+void rebuildChunkMesh(Chunk& chunk);
